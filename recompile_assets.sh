03a04716514221c8dc9a74800d597617fb00b556
Yuuko <aznrogerazn@gmail.com>
2023-04-24 18:17:54 +0800

    feat: adding convenient script for assets

---
+++ recompile_assets.sh
@@ -0,0 +1,5 @@
+cd apps/block_scout_web/assets; npm install && node_modules/webpack/bin/webpack.js --mode production; cd -
+
+mix phx.digest
+
+echo "Webpack and phx.digest done. Please start server."
